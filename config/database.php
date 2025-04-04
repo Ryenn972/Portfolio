95c9bea6eed9dd86bd61c2a47d4b48ecc419a637
Ryenn <ryenn.mith-catan@protonmail.com>
2025-04-04 04:37:47 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 function getConnexion():object
 {
-    $pdo = new PDO('mysql:host=db.3wa.io;port=3306;dbname=ryennmithcatan_portfolio;charset=utf8', 'ryennmithcatan', 'ab09fc793df41efc7e15ac8eacb44015');
+    $pdo = new PDO('mysql:host=;port=;dbname=;charset=', '', '');
     
     return $pdo;
 }
